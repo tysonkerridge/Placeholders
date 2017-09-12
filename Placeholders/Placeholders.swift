aa234236b508e2e70582f766b8744ace12071e21
Tyson Kerridge <tysonbehere@hotmail.com>
2017-09-12 14:01:53 +1000

    Updated the Placeholder class with a parameter which allows the creator of the placeholders to skip the first item. It gets used once and then deleted if it's set, and is nil by default in the `start(...)` call. Useful for in the case where you might already have a placeholder showing, eg. "Do something", and that same placeholder is one of the elements which might be chosen to be displayed first. It's a slight edge case, but possible in cases where the placeholders are shuffled or set up where the first one is the static one set first.

--- Placeholders/Placeholders.swift
+++ Placeholders/Placeholders.swift
@@ -27,6 +27,11 @@ final public class Placeholders<Element> {
     var timer: Timer?
     var action: (Element) -> () = { _ in }
     
+    /// Used to check if the first element should be skipped, for reasons where the first element might already be showing and we should skip it.
+    /// Mainly useful for cases where an infinite shuffle is used as any placeholder could be used first and already be showing.
+    /// This is used once in the first timer handler if set via the `start(...)` method and then deleted and never set or called again.
+    private var shouldSkipFirstElement: ((Element) -> Bool)?
+
     public init<Iterator : IteratorProtocol>(iterator: Iterator) where Iterator.Element == Element {
         self.iterator = AnyIterator(iterator)
     }
@@ -45,7 +50,9 @@ final public class Placeholders<Element> {
         timer?.invalidate()
     }
     
-    public func start(interval: TimeInterval, fireInitial: Bool, action: @escaping (Element) -> ()) {
+    /// - Parameter shouldSkipFirstElement: A closure which sends the first placeholder to show, to decide whether it should be shown. Return `true` if the element passed is the same as the element already showing.
+    public func start(interval: TimeInterval, fireInitial: Bool, shouldSkipFirstElement: ((Element) -> Bool)? = nil, action: @escaping (Element) -> ()) {
+        self.shouldSkipFirstElement = shouldSkipFirstElement
         self.action = action
         let timer = Timer(timeInterval: interval,
                           target: self,
@@ -56,17 +63,50 @@ final public class Placeholders<Element> {
         self.timer = timer
         if fireInitial {
             if let firstPlaceholder = iterator.next() {
-                action(firstPlaceholder)
+                actionIfNeeded(firstPlaceholder)
             }
         }
     }
     
     @objc private func act(timer: Timer) {
-        if let nextPlaceholder = iterator.next() {
-            action(nextPlaceholder)
-        } else {
+
+        // Bail, invalidating the timer, if there's no more placeholders
+        guard let nextPlaceholder = iterator.next() else {
             timer.invalidate()
+            return
         }
+
+        // Action the placeholder
+        actionIfNeeded(nextPlaceholder)
+
     }
     
+    private func actionIfNeeded(_ placeholder: Element) {
+
+        // If there's a check to skip, we need to do it and remove the reference before we can continue
+        if let shouldSkipFirstElement = self.shouldSkipFirstElement {
+            self.shouldSkipFirstElement = nil
+
+            // Now that we've removed the reference, do the check
+            // If we shouldn't skip, we can continue and action the placeholder, otherwise bail to skip it and get the next one instead
+            guard !shouldSkipFirstElement(placeholder) else {
+
+                // If there's a timer, use it's action to invalidate now if no more placeholders
+                if let timer = self.timer {
+                    act(timer: timer)
+
+                    // Otherwise action the next placeholder, if any
+                } else if let secondPlaceholder = iterator.next() {
+                    actionIfNeeded(secondPlaceholder)
+                }
+                return
+            }
+
+        }
+
+        // No check to skip, or it passed and we didn't need to skip, so action the placeholder
+        action(placeholder)
+
+    }
+
 }
